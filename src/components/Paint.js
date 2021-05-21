a7d6119a142fbf99158048af765ffead8372b793
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-05-21 23:14:34 +0900

    Add canvas and title about paint component

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Paint.css';
 
 function Paint() {
   return (
     <div className="paint">
-      <header className="paint-header">
-        <span>Hello MoP!</span>
+      <header className="paint-title">
+        <h1>Palette</h1>
       </header>
+      <canvas className="canvas" />
     </div>
   );
 }
