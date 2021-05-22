d25e66986e8fedf8b4a4927f7d3cdba368e624cd
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-05-22 22:05:27 +0900

    Add colors

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -8,6 +8,19 @@ function Paint() {
         <h1>Palette</h1>
       </header>
       <canvas className="canvas" />
+      <div className="controls">
+        <div className="controls__colors">
+          <div className="controls__color controls__black" />
+          <div className="controls__color controls__white" />
+          <div className="controls__color controls__red" />
+          <div className="controls__color controls__orange" />
+          <div className="controls__color controls__yellow" />
+          <div className="controls__color controls__green" />
+          <div className="controls__color controls__skyblue" />
+          <div className="controls__color controls__blue" />
+          <div className="controls__color controls__purple" />
+        </div>
+      </div>
     </div>
   );
 }
