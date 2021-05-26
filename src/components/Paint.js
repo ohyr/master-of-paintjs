7f49aebe1350b8adf1955c6d93b6994feab85801
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-05-26 17:15:20 +0900

    Can Paint!

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -1,16 +1,48 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Paint.css';
 
 function Paint() {
   const canvasRef = useRef();
+  const contextRef = useRef();
+
+  const [ctx, setCtx] = useState();
+  const [painting, setPainting] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     canvas.width = 700;
     canvas.height = 700;
 
+    const context = canvas.getContext('2d');
+    context.strokeStyle = 'black';
+    context.lineWidth = 2.5;
+    contextRef.current = context;
+
+    setCtx(context);
     return () => {};
   }, []);
+
+  function startPainting() {
+    setPainting(true);
+  }
+
+  function stopPainting() {
+    setPainting(false);
+  }
+
+  function onPainting({ nativeEvent }) {
+    const { offsetX, offsetY } = nativeEvent;
+
+    if (ctx) {
+      if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(offsetX, offsetY);
+      } else {
+        ctx.lineTo(offsetX, offsetY);
+        ctx.stroke();
+      }
+    }
+  }
 
   function handleSaveClick() {
     const image = canvasRef.current.toDataURL();
@@ -25,7 +57,14 @@ function Paint() {
       <header className="paint-title">
         <h1>Palette</h1>
       </header>
-      <canvas className="canvas" ref={canvasRef} />
+      <canvas
+        className="canvas"
+        ref={canvasRef}
+        onMouseDown={startPainting}
+        onMouseUp={stopPainting}
+        onMouseMove={onPainting}
+        onMouseLeave={stopPainting}
+      />
       <div className="controls">
         <div className="controls__range">
           <input
