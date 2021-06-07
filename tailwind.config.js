69ce24c422c0dca12cae2cc6e0a0b7a5f25c3ce8
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-06-07 22:39:00 +0900

    Set tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
+    options: {
+      keyframes: true,
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
